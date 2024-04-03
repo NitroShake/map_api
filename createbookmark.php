01808c3a7e8eefc1f204fd814fdc66012560fc72
NitroShake <63812175+NitroShake@users.noreply.github.com>
2024-04-03 18:43:09 +0100

    deletebookmark functional + createbookmark can't create a duplicate

--- createbookmark.php
+++ createbookmark.php
@@ -19,14 +19,23 @@
         }
 
         $id = $stmt->fetch(PDO::FETCH_ASSOC)['id'];
-        $stmt = $pdo->prepare("INSERT INTO bookmarks (locations_id, accounts_id) VALUES (?, ?);");
-        $success = $stmt->execute([$id, $user['id']]);
-    
-        if ($success) {
-            http_response_code(200);
-        } else {
-            http_response_code(3);
+
+        $stmt = $pdo->prepare("SELECT * FROM bookmarks WHERE locations_id = ? AND accounts_id = ?;");
+        if (!$stmt->execute([$id, $user['id']])) {
+            http_response_code(3); die;
         }
+
+        if ($stmt->rowCount() == 0) {
+            $stmt = $pdo->prepare("INSERT INTO bookmarks (locations_id, accounts_id) VALUES (?, ?);");
+            $success = $stmt->execute([$id, $user['id']]);
+        
+            if ($success) {
+                http_response_code(200);
+            } else {
+                http_response_code(4);
+            }
+        }
+
     } catch (Exception $e) {
-        echo $e->getMessage();
+        http_response_code(5);
     }
--- deletebookmark.php
+++ deletebookmark.php
@@ -1,11 +1,23 @@
 <?php
     require_once("auth.php");
     $user = verifyUser($pdo);
+    echo 'e';
 
-    $stmt = $pdo->prepare("DELETE * FROM bookmarks WHERE accounts_id = ? AND locations_id = ?;");
-    if (!$stmt->execute([$user['id'], $_GET['locationid']])) {
+    $stmt = $pdo->prepare("SELECT * FROM locations WHERE osm_id = ? AND osm_type = ?;");
+    if (!$stmt->execute([$_GET['osm_id'], $_GET['osm_type']])) {
         http_response_code(0); die;
     }
-    else {
-        http_response_code(200);
+
+    if ($stmt->rowCount() == 0) {
+        http_response_code(0); die;
     }
+
+    $locationId = $stmt->fetch(PDO::FETCH_ASSOC)['id'];
+
+    $stmt = $pdo->prepare("DELETE FROM bookmarks WHERE accounts_id = ? AND locations_id = ?;");
+    if (!$stmt->execute([$user['id'], $locationId])) {
+        http_response_code(1); die;
+    }
+
+    http_response_code(200);
+    
